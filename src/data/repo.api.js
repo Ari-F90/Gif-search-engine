859e31f87c673d9b60a59bb88037e5ce274a6097
Ariadna Frias <ariadna.812@gmail.com>
2023-08-28 23:09:49 +0200

    Change limit img in repo

--- src/data/repo.api.js
+++ src/data/repo.api.js
@@ -1,12 +1,12 @@
 export const getGifs = async (field) => {
   const apiKey = "9wfm5RVdeDzS85Ukzzw8jjmWsb72PZsh";
-  const urlApi = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${field}&limit=10`;
+  const urlApi = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${field}&limit=6`;
   const resp = await fetch(urlApi);
   const { data } = await resp.json();
   const gifs = data.map((img) => ({
     id: img.id,
     title: img.title,
-    url: img.images.downsized_medium.url,
+    url: img.images.downsized.url,
   }));
   return gifs;
 };
